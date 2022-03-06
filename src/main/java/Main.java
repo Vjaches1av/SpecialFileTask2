114faf3d9dd1fd2d81349ac5f0ab6c199389720a
Vyacheslav <informationsss@ya.ru>
2022-03-06 23:20:50 +0300

    Задача выполнена

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,53 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.w3c.dom.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
-import java.lang.reflect.Type;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public final class Main {
 
+    private static Employee parseEmployee(Node employee) {
+        long id             = 0L;
+        String firstName    = "";
+        String lastName     = "";
+        String country      = "";
+        int age             = 0;
+
+        NodeList children = employee.getChildNodes();
+        for (int j = 0; j < children.getLength(); j++) {
+            if (children.item(j).getNodeType() == Node.ELEMENT_NODE) {
+                Node node = children.item(j);
+                String name = node.getNodeName();
+                String value = node.getFirstChild().getNodeValue();
+                switch (name) {
+                    case ("id"):        id          = Long.parseLong(value);    break;
+                    case ("firstName"): firstName   = value;                    break;
+                    case ("lastName"):  lastName    = value;                    break;
+                    case ("country"):   country     = value;                    break;
+                    case ("age"):       age         = Integer.parseInt(value);  break;
+                }
+            }
+        }
+        return new Employee(id, firstName, lastName, country, age);
+    }
+
+    private static List<Employee> xmlToListEmployee(File file) throws Exception {
+        List<Employee> list = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(file);
+        Element root = document.getDocumentElement();
+        NodeList employees = root.getElementsByTagName("employee");
+        for (int i = 0; i < employees.getLength(); i++)
+            list.add(parseEmployee(employees.item(i)));
+        return list;
+    }
+
     private static <T> String listObjToJson(List<T> list) {
-        Type listType = new TypeToken<List<T>>() {}.getType();
-        return new Gson().toJson(list, listType);
+        return new Gson().toJson(list, new TypeToken<List<T>>() {}.getType());
     }
 
     public static void main(String[] args) {
@@ -19,13 +56,12 @@ public final class Main {
             File file = new File(scanner.nextLine());
             try {
                 if (file.exists() && file.isFile() && file.getName().endsWith(".xml")) {
-
-                } else {
-                    throw new FileNotFoundException();
-                }
+                    List<Employee> employees = xmlToListEmployee(file);
+                    System.out.println(listObjToJson(employees));
+                } else throw new FileNotFoundException();
             } catch (FileNotFoundException e) {
                 System.err.println("Проверьте правильность указания пути и имени файла.");
-            } catch (IOException e) {
+            } catch (Exception e) {
                 System.err.println("Произошла ошибка при попытке чтения указанного файла.");
             }
         }
